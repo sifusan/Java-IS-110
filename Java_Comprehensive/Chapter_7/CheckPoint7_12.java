abdee9cd4e6a4d7996ebb3a87137de18850d7f6d
sifusan <fuglestad.simen@gmail.com>
2015-02-26 17:33:14 +0100

    Finished chapter 7

--- Java_Comprehensive/Chapter_7/CheckPoint7_12.java
+++ Java_Comprehensive/Chapter_7/CheckPoint7_12.java
@@ -1,9 +1,9 @@
-public class DeckOfCards {
+public class CheckPoint7_12 {
     public static void main(String[] args) {
         int[] deck = new int[52];
         String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        String[] ranks = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-            "Jack", "Queen", "King"};
+        String[] ranks = ("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "Jack", "Queen", "king"};
         
         for (int i = 0; i < deck.length; i++)
             deck[i] = i;
@@ -15,12 +15,12 @@ public class DeckOfCards {
             deck[index] = temp;
         }
         
-        for (int i = 0; i < 4; i ++) {
-            String suit = suits[deck[i] / 13];
-            String rank = ranks[deck[i] % 13];
-            System.out.println("Card number " + deck[i] + ": " + rank +
-                " of " + suit);
+        for (int i = 0; i < 4; i++) {
+            int cardNumber  = (int)(Math.random() * deck.length);
+            String suit = suits[cardNumber / 13];
+            String rank = ranks[cardNumber % 13];
+            System.out.println("Card number " + cardNumber + ": " +
+                rank + " of " + suit);
         }
-
     }
-}        
+}
--- Java_Comprehensive/Chapter_7/DeckOfCards.java
+++ Java_Comprehensive/Chapter_7/DeckOfCards.java
@@ -2,7 +2,7 @@ public class DeckOfCards {
     public static void main(String[] args) {
         int[] deck = new int[52];
         String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-        String[] ranks = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
             "Jack", "Queen", "King"};
         
         for (int i = 0; i < deck.length; i++)
