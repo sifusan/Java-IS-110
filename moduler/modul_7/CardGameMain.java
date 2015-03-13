7116dfaf46cd90f8adfcacb7c82be727c67c3f4c
sifusan <fuglestad.simen@gmail.com>
2015-03-13 11:46:48 +0100

    Mroe stuff

---
+++ moduler/modul_7/CardGameMain.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class CardGameMain {
+    private Deck Deck;
+    
+    public CardGameMain() {
+        
+        CardGameMain newGame = new CardGameMain();
+    }
+    
+    
+    public static void main(String[] args) {
+        Deck newDeck = new Deck();
+        newDeck.getDeck();
+    }
+}
